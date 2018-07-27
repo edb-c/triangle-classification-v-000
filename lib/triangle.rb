fbf7a9c2cd202676431dedfffbdf3846b32aab9d
Ethel C <cofresi.ethel@gmail.com>
2018-07-27 15:41:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,16 +21,11 @@ class Triangle
     end
   end
 
-  def kind
-
-    check_triangle
-
    def check_triangle
     real_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
     [side1, side2, side3].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
    end
-  end
 
   class TriangleError < StandardError
   end
